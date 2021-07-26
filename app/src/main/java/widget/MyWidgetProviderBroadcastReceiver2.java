ce32cd613219bca5c100b3c8a10311c1eee5aff6
BIB ALI <bibali1980@gmail.com>
2021-07-26 21:52:29 +0100

    Update MyWidgetProviderBroadcastReceiver2.java

--- app/src/main/java/widget/MyWidgetProviderBroadcastReceiver2.java
+++ app/src/main/java/widget/MyWidgetProviderBroadcastReceiver2.java
@@ -27,7 +27,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.widget.RemoteViews;
 
-import com.sally.R;
+import com.sallyprayertimes.R;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
